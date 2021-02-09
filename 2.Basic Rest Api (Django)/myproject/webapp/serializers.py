cd276362265d56e9dae970c065d1d778de725ae6
Adarsh kandwal <41911887+AdarshKandwal@users.noreply.github.com>
2021-02-09 17:19:09 +0530

    Add files via upload

---
+++ 2.Basic Rest Api (Django)/myproject/webapp/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from . models import employees
+
+class employeesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=employees
+        fields='__all__'
+         
