dadc3a0f2b00ad0bd1c7f48cab5092c0d0a762ee
Adarsh kandwal <41911887+AdarshKandwal@users.noreply.github.com>
2021-02-14 19:02:29 +0530

    Delete serializers.py

--- 2.RestApi/1.BasicRestApi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import employees
-
-class employeesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=employees
-        fields='__all__'
-         
